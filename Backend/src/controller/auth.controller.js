473f0df4a3ed3ab3d52a0bb0955ff1d3d3984239
neerajjha0426 <neerajjha807611@gmail.com>
2025-10-05 16:52:12 +0530

    Login Logout functions added

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -79,3 +79,43 @@ export const signup = async (req, res) => {
    }
 
 }
+
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        // Basic validation check for empty fields
+        if(!email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid email or password"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message:"Invalid email or password"});
+        }
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+            message:"Login successful"
+        });
+    } catch (error) {
+        console.error('Login error:', error);
+        res.status(500).json({message:"Server Error"});
+    }
+};
+
+
+
+export const logout = (req, res) => {
+    res.cookie('jwt', '', {maxAge:0});
+    res.status(200).json({message:"Logout successful"});
+};
