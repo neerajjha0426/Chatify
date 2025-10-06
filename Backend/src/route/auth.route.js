9d60a7234da3842b1eb406fa2a6d58babd29e55a
neerajjha0426 <neerajjha807611@gmail.com>
2025-10-06 23:55:40 +0530

    Added Rate Limiting and Protection from Bots

--- Backend/src/route/auth.route.js
+++ Backend/src/route/auth.route.js
@@ -2,12 +2,15 @@ import express from 'express';
 import User from '../models/User.js';
 import { signup ,login ,logout,updateProfile } from '../controller/auth.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
+import { arcjetProtection } from '../middleware/arcjet.middleware.js';
 
 const router = express.Router();
 
+router.use(arcjetProtection);// Apply Arcjet protection to all routes in this router
+
 router.post('/signup',signup);
 
-router.post('/login', login);
+router.post('/login',arcjetProtection, login);
 
 router.post('/logout', logout);
 
