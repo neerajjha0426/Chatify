512eef8a5b1136b4eb4e5fe1e24d3f2c716bd711
neerajjha0426 <neerajjha807611@gmail.com>
2025-10-08 22:09:01 +0530

    added messages endpoint

--- Backend/src/route/message.route.js
+++ Backend/src/route/message.route.js
@@ -1,9 +1,16 @@
-
+import { getAllContacts,getChatPartners,getMessagesByUserId,sendMessage} from '../controller/message.controller.js';
 import express from 'express';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { arcjetProtection } from '../middleware/arcjet.middleware.js';
 const router = express.Router();
 
-router.get('/send', (req, res) => {
-    res.send('send message page!');
-});
+
+
+router.use(arcjetProtection,protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
